c88dad901ea2bf18fc648a970ea601eb6aaa3915
Iuri Matias <iuri.matias@gmail.com>
2018-10-23 14:54:13 -0400

    re-style contracts sidebar

--- embark-ui/src/components/ContractOverview.js
+++ embark-ui/src/components/ContractOverview.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import {
   Row,
   Col,
+  Form,
   FormGroup,
   Label,
   Input,
@@ -12,18 +13,24 @@ import {
   CardHeader,
   CardTitle,
   CardFooter,
+  Collapse,
   ListGroup,
   ListGroupItem
 } from "reactstrap";
+import {formatContractForDisplay} from '../utils/presentation';
 
 class ContractFunction extends Component {
   constructor(props) {
     super(props);
-    this.state = {inputs: {}};
+    this.state = {inputs: {}, optionsCollapse: false, functionCollapse: false};
   }
 
   static isPureCall(method) {
     return (method.mutability === 'view' || method.mutability === 'pure');
+  }
+
+  static isEvent(method) {
+    return !this.isPureCall(method) && (method.type === 'event');
   }
 
   buttonTitle() {
@@ -56,39 +63,68 @@ class ContractFunction extends Component {
     return this.inputsAsArray().length !== this.props.method.inputs.length;
   }
 
+  toggleOptions() {
+    this.setState({
+      optionsCollapse: !this.state.optionsCollapse,
+    });
+  }
+
+  toggleFunction() {
+    this.setState({
+      functionCollapse: !this.state.functionCollapse,
+    });
+  }
+
   render() {
     return (
-      <Col xs={12}>
         <Card>
           <CardHeader>
-            <CardTitle>{this.props.method.name}</CardTitle>
+            <CardTitle className="collapsable contractFunction" onClick={() => this.toggleFunction()}>
+              {ContractFunction.isPureCall(this.props.method) &&
+                <button class="btn btn-brand btn-sm" style={{ "color": "#fff", "background-color": "#ff4500", "border-color": "#ff4500", "float": "right" }}>call</button>
+              }
+              {ContractFunction.isEvent(this.props.method) &&
+                <button class="btn btn-brand btn-sm" style={{ "color": "#fff", "background-color": "#aad450", "border-color": "#aad450", "float": "right" }}>event</button>
+              }
+              {this.props.method.name}({this.props.method.inputs.map(input => input.name).join(', ')})
+            </CardTitle>
           </CardHeader>
-          <CardBody>
-            {(contractDisplay.state === 'Deployed') && <div>Deployed at {contractDisplay.address}</div>}
-            {(contractDisplay.state !== 'Deployed') && <div>{contractDisplay.address}</div>}
-            <Form action="" method="post" inline>
-            {this.props.method.inputs.map(input => (
-              <FormGroup key={input.name}>
-                <Label for={input.name}>{input.name}</Label>
-                <Input name={input.name} id={input.name} placeholder={input.type} onChange={(e) => this.handleChange(e, input.name)}/>
-              </FormGroup>
-            ))}
-            </Form>
-            {!ContractFunction.isPureCall(this.props.method) &&
-              <div>
-                <strong>Details</strong>
-                <Form action="" method="post" inline>
-                  <FormGroup key="gasPrice">
-                    <Label for="gasPrice">Gas Price (in GWei)(optional)</Label>
-                    <Input name="gasPrice" id="gasPrice" onChange={(e) => this.handleChange(e, 'gasPrice')}/>
-                  </FormGroup>
-                </Form>
+          <Collapse isOpen={this.state.functionCollapse}>
+            <CardBody>
+              <Form action="" method="post" inline>
+              {this.props.method.inputs.map(input => (
+                <FormGroup key={input.name} className="pr-1">
+                  <Label for={input.name} className="pr-1">{input.name}: </Label>
+                  <Input name={input.name} id={input.name} placeholder={input.type} onChange={(e) => this.handleChange(e, input.name)}/>
+                </FormGroup>
+              ))}
+              </Form>
+              {!ContractFunction.isPureCall(this.props.method) &&
+                <Col xs={12} style={{"margin-bottom": "5px", "margin-top": "5px"}}>
+                  <Row>
+                  <strong className="collapsable" onClick={() => this.toggleOptions()}><i className={this.state.optionsCollapse ? 'fa fa-caret-down' : 'fa fa-caret-right'}/>Advanced Options</strong>
+                    <Col xs={12} style={{"margin-bottom": "5px", "margin-top": "5px"}}>
+                      <Row>
+                        <Collapse isOpen={this.state.optionsCollapse}>
+                          <Form action="" method="post" inline>
+                            <FormGroup key="gasPrice" className="pr-1">
+                              <Label for="gasPrice" className="pr-1">Gas Price (in GWei)(optional)</Label>
+                              <Input name="gasPrice" id="gasPrice" onChange={(e) => this.handleChange(e, 'gasPrice')}/>
+                            </FormGroup>
+                          </Form>
+                        </Collapse>
+                     </Row>
+                   </Col>
+                 </Row>
+               </Col>
+              }
+              <div align="right">
+                <Button color="primary" disabled={this.callDisabled()} onClick={(e) => this.handleCall(e)}>
+                  {this.buttonTitle()}
+                </Button>
               </div>
-            }
-            <Button color="primary" disabled={this.callDisabled()} onClick={(e) => this.handleCall(e)}>
-              {this.buttonTitle()}
-            </Button>
-          </CardBody>
+            </CardBody>
+          </Collapse>
           {this.props.contractFunctions && this.props.contractFunctions.length > 0 && <CardFooter>
             <ListGroup>
               {this.props.contractFunctions.map(contractFunction => (
@@ -100,7 +136,6 @@ class ContractFunction extends Component {
             </ListGroup>
           </CardFooter>}
         </Card>
-      </Col>
     );
   }
 }
@@ -119,10 +154,14 @@ const filterContractFunctions = (contractFunctions, contractName, method) => {
 };
 
 const ContractOverview = (props) => {
-  const {contractProfile} = props;
+  const {contractProfile, contract} = props;
+  const contractDisplay = formatContractForDisplay(contract);
 
   return (
-    <Row>
+    <div>
+      {(contractDisplay.state === 'Deployed') && <div>Deployed at {contractDisplay.address}</div>}
+      {(contractDisplay.state !== 'Deployed') && <div>{contractDisplay.address}</div>}
+      <br />
       {contractProfile.methods
         .filter((method) => {
           return props.onlyConstructor ? method.type === 'constructor' : method.type !== 'constructor';
@@ -132,11 +171,12 @@ const ContractOverview = (props) => {
                                          contractFunctions={filterContractFunctions(props.contractFunctions, contractProfile.name, method.name)}
                                          contractProfile={contractProfile}
                                          postContractFunction={props.postContractFunction}/>)}
-    </Row>
+    </div>
   );
 };
 
 ContractOverview.propTypes = {
+  contract: PropTypes.object,
   onlyConstructor: PropTypes.bool,
   contractProfile: PropTypes.object,
   contractFunctions: PropTypes.arrayOf(PropTypes.object),
